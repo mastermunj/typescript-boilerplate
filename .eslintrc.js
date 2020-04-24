bc8f2bf5ae5ec42ac8656ce45018650d51855dfe
Munjal Dhamecha <mastermunj@gmail.com>
2020-04-24 11:49:16 +0530

    Initial Commit

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  plugins: [
+    '@typescript-eslint',
+  ],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  env: {
+    es6: true,
+    node: true,
+    commonjs: true,
+    browser: false,
+  },
+};
