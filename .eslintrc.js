c91f2e261e43ccdd7261a982afa953455fa4c605
Munjal Dhamecha <mastermunj@gmail.com>
2020-04-24 13:40:35 +0530

    Added prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,6 +8,8 @@ module.exports = {
     'eslint:recommended',
     'plugin:@typescript-eslint/eslint-recommended',
     'plugin:@typescript-eslint/recommended',
+    'prettier',
+    'prettier/@typescript-eslint',
   ],
   env: {
     es6: true,
